c2648131ba2b9ace3dbd9add964f40d672287cff
ANDERSON DE OLIVEIRA E SILVA JUNIOR <87553205+Anderson-OSJr@users.noreply.github.com>
2025-01-11 18:57:40 -0300

    My first commit, after Navbar setup

---
+++ src/components/navbar/NavLink.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { NavbarItem } from "@nextui-org/react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+type NavLinkProps = {
+  href: string;
+  label: string;
+};
+
+const NavLink = ({ href, label }: NavLinkProps) => {
+  const pathname = usePathname();
+  return (
+    <>
+      <NavbarItem isActive={pathname === href} as={Link} href={href}>
+        {label}
+      </NavbarItem>
+    </>
+  );
+};
+export default NavLink;
